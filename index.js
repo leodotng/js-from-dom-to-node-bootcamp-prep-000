dabc493b7574ba94925c79e250e8ca9d212325a5
Josh Leong <leong.jo@gmail.com>
2017-09-21 06:33:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 // write your code below!
+var dog = "thirsty"
+
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+} else {
+  console.log("Reading newspaper.")
+}
+
+
+
+
+
+
+
+
 // if (6 + 3 === 9) {
 //   console.log("Giraffes have no vocal cords.")
 // }
 
-// 
+//
 // if (6 + 3 < 5) {
 //   console.log("The hummingbird is the only animal that can fly backwards")
 // }
 
 
-var dog = "satisfied"
+// var dog = "satisfied"
+// 
+// if (dog === "hungry") {
+//   console.log("Refilling food bowl.")
+// } else {
+//   console.log("Reading newspaper.")
+// }
 
-if (dog === "hungry") {
-  console.log("Refilling food bowl.")
-} else {
-  console.log("Reading newspaper.")
-}
+
