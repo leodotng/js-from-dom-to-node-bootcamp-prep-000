65c62c40382e3d2db42fdd1b2fa457585c3e2519
Josh Leong <leong.jo@gmail.com>
2017-09-21 06:30:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // write your code below!
-if (6 + 3 === 9) {
-  console.log("Giraffes have no vocal cords.")
+// if (6 + 3 === 9) {
+//   console.log("Giraffes have no vocal cords.")
+// }
+
+
+if (6 + 3 < 5) {
+  console.log("The hummingbird is the only animal that can fly backwards")
 }
